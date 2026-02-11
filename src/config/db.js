e757289f98cd62c104b0dce4b2448f720b281218
hussam-alhor <hosam4255@gmail.com>
2026-02-11 15:29:27 +0200

    11/2 db

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const seedAdmin = require("../helpers/seedAdmin");
 // MONGO_URI_LOCAL
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_LOCAL);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     
